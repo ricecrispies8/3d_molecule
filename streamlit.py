e22a37d777954322d0d40e274632f856ee750ed3
Kyle <kylelombardo888@gmail.com>
2023-01-09 19:40:23 -0700

    suggested some things to add in the future

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,15 @@
 from molecule_3d import Chem_API 
 import streamlit as st
+import random
+
+"""
+Things to add:
+* Random Compound Name generator
+* Improve introduction
+* Add profile page with connection to this and other pages
+
+
+"""
 
 st.markdown('''
 # 3D Compound Visualization
@@ -13,18 +23,23 @@ Watch for additional features in the near future!
 
 compound_name = st.text_input('Enter in the name of a compound:', 'Fructose')
 
+random_cid = random.randint(0,100)
+suggestions = f
+
+st.markdown("_Not sure what ")
+
 compound = Chem_API(compound_name, 'name')
 
 tab1, tab2 = st.tabs(["Chart", "Data"])
 
 with tab1.container():
-    st.header(f'{compound.name} Plot')
+    st.header(f'{compound.name.capitalize()} Plot')
     st.markdown("_Go full-screen using the arrows above the legend._")
 
     with st.spinner("Your molecule is loading..."):
         st.plotly_chart(compound.plot_3d())
 
 with tab2.container():
-    st.header(f'{compound.name} Data')
+    st.header(f'{compound.name.capitalize()} Data')
     with st.spinner("Your molecule is loading..."):
         st.dataframe(compound.atomic_df())
