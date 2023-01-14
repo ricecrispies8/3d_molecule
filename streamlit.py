be8be331e3f1e157b89b707882f47b27cd96b467
Kyle <kylelombardo888@gmail.com>
2023-01-13 18:12:52 -0700

    fixed plotly bug where it was not rendering well in mobile

--- streamlit.py
+++ streamlit.py
@@ -1,32 +1,17 @@
 from molecule_3d import Chem_API 
 import streamlit as st
-import random
-
-"""
-Things to add:
-* Random Compound Name generator
-* Improve introduction
-* Add profile page with connection to this and other pages
-
-
-"""
 
 st.markdown('''
 # 3D Compound Visualization
 
 Being able to visualize a compound is essential for understanding how a molecule functions. This simple tool will show the molecule's bonds and coordinates for a typical orientation of a molecule. 
 
-This project uses the [PubChem API](https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest) to obtain the cooridinates for any of the 113 million compounds listed in it's database. 
+This project uses the [PubChem API](https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest) to obtain the cooridinates for any of the 114 million compounds listed in it's database. 
 
 Watch for additional features in the near future!
  ''')
 
 compound_name = st.text_input('Enter in the name of a compound:', 'Fructose')
-
-random_cid = random.randint(0,100)
-suggestions = f
-
-st.markdown("_Not sure what ")
 
 compound = Chem_API(compound_name, 'name')
 
@@ -37,7 +22,7 @@ with tab1.container():
     st.markdown("_Go full-screen using the arrows above the legend._")
 
     with st.spinner("Your molecule is loading..."):
-        st.plotly_chart(compound.plot_3d())
+        st.plotly_chart(compound.plot_3d(), use_container_width=True)
 
 with tab2.container():
     st.header(f'{compound.name.capitalize()} Data')
